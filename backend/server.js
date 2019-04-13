37540853400db9abfc924fed2574157066d9f5bd
Qbisst <ismaelaperez@outlook.com>
2019-04-13 11:13:54 -0400

    table and waiting lists

--- backend/server.js
+++ backend/server.js
@@ -13,3 +13,24 @@ app.use(express.json());
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
+
+// variables to hold table and waiting list data//
+
+var tableArray = [
+  {
+    id: "iperez",
+    name: "Ismael Perez",
+    email: "iperez@somewhere.com",
+    phone: "000-000-0000"
+  }
+
+];
+
+var waitingArray = [
+  {
+    id: "iperez",
+    name: "Ismael Perez",
+    email: "iperez@somewhere.com",
+    phone: "000-000-0000"
+  }
+];
